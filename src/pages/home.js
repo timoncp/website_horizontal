1c49b69d82a326ef8ae830f7302dfcaf997eb5ed
timoncp <timon.carneciprove@icloud.com>
2019-05-26 18:19:39 +0300

    Fixed home on all devices

--- src/pages/home.js
+++ src/pages/home.js
@@ -15,8 +15,10 @@ function Home() {
         <p className='welcome-yellow'>bland developer website</p>
         <p className='welcome-gray'>in the world.</p>
       </div>
-      <img id='home-arrow' src={ArrowSVG} alt='arrow pointing towards title' />
-      <span id='home-arrow-text'>*At least that’s what I’ve been told about my color scheme</span>
+      <div class='home-arrow-container'>
+        <img id='home-arrow' src={ArrowSVG} alt='arrow pointing towards title' />
+        <span id='home-arrow-text'>*At least that’s what I’ve been told about my color scheme</span>
+      </div>
       <div className='home-socials'>
         <SocialBtn type='linkedinWhite' />
         <SocialBtn type='githubWhite' />
